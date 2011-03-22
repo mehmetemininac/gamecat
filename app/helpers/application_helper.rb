b2282d2c1b7f308a3d89a0c79894ec596f0481f4
Rayko <rayko.drg@gmail.com>
2011-03-21 21:31:00 -0300

    Major update in helpers and updated container for the new helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
+
   def link_icon image=nil
     size = 17
     icon_set = 'led_icons'
@@ -8,27 +8,27 @@ module ApplicationHelper
       image_tag "icons/#{icon_set}/normal/#{image}", :mouseover => "icons/#{icon_set}/hoover/#{image}", :width => size, :height => size
     end
   end
-  
+
   def new_icon
     link_icon "add.png"
   end
-  
+
   def home_icon
     link_icon 'home.png'
   end
-  
+
   def edit_icon
     link_icon 'edit.png'
   end
-  
+
   def show_icon
     link_icon 'detail.png'
   end
-  
+
   def remove_icon
     link_icon 'remove.png'
   end
-  
+
   def view_all_icon
     link_icon 'view_all.png'
   end
@@ -36,53 +36,70 @@ module ApplicationHelper
   def back_icon
     link_icon 'back.png'
   end
-  
+
   def next_icon
     link_icon 'next.png'
   end
-  
+
   def previous_icon
     link_icon 'previous.png'
   end
-  
+
   def edit_discs_icon
     link_icon 'edit_discs.png'
   end
 
-  def common_links section=nil, object=nil
+  def common_links options={ } # section=nil, object=nil, klass=nil
+    section, object, klass = options[:section], options[:object], options[:klass]
     unless section.nil?
       manual_edit_path = 'edit_' + object.class.to_s.downcase + '_path(object)'
       if section == :list and !object.nil?
-        "<td> #{link_to(show_icon, url_for(object))} </td>
-        <td> #{link_to(edit_icon, eval(manual_edit_path))} </td>
-        <td> #{link_to(remove_icon, url_for(object), :confirm => 'Are you sure?', :method => :delete)} </td>"
+        result = ""
+        if can? :read, object
+          result += "<td> #{link_to(show_icon, url_for(object))} </td>"
+        end
+        if can? :edit, object
+          result += "<td> #{link_to(edit_icon, eval(manual_edit_path))} </td>"
+        end
+        if can? :destroy, object
+          result += "<td> #{link_to(remove_icon, url_for(object), :confirm => 'Are you sure?', :method => :delete)} </td>"
+        end
       elsif section == :main
-        "<p> #{link_to(new_icon, url_for(:action => 'new'))} 
-        #{link_to(home_icon, root_path)} </p>"
+        result = "<p>"
+        if can? :create, klass
+          result += link_to(new_icon, url_for(:action => 'new'))
+        end unless klass.nil?
+        result += "#{link_to(home_icon, root_path)} </p>"
       elsif section == :show
-        "<p>
-          #{link_to(edit_icon, eval(manual_edit_path))}
-          #{link_to(remove_icon, url_for(object), :confirm => 'Are you sure?', :method => :delete)}
-          #{link_to(back_icon, url_for(:action => 'index'))}
-        </p>"
+        result = "<p>"
+        if can? :edit, object
+          result += link_to(edit_icon, eval(manual_edit_path))
+        end
+        if can? :destroy, object
+          result += link_to(remove_icon, url_for(object), :confirm => 'Are you sure?', :method => :delete)
+        end
+        result += "#{link_to(back_icon, url_for(:action => 'index'))} </p>"
       elsif section == :edit
-        "<p>
-          #{link_to(show_icon, object)}
-          #{link_to(back_icon, url_for(:action => 'index'))}
-        </p>"
+        result = "<p>"
+        if can? :read, object
+          result += link_to(show_icon, object)
+        end
+          result += "#{link_to(back_icon, url_for(:action => 'index'))} </p>"
       elsif section == :new
-        "<p>
+        result = "<p>
         #{link_to(back_icon, url_for(:action => 'index'))}
         </p>"
       elsif section == :edit_discs
         manual_edit_discs_path = 'edit_discs_path(object)'
-        "<p>
-          #{link_to(edit_icon, eval(manual_edit_path))}
+        result = "<p>"
+        if can? :edit, object
+          result += "#{link_to(edit_icon, eval(manual_edit_path))}
           #{link_to(edit_discs_icon, eval(manual_edit_discs_path))}
           #{link_to(remove_icon, url_for(object), :confirm => 'Are you sure?', :method => :delete)}
-          #{link_to(back_icon, url_for(:action => 'index'))}
-        </p>"
+          #{link_to(back_icon, url_for(:action => 'index'))} </p>"
+        end
       end
+      return result
     end
   end
 end
